5fcbc396de118a9bf111acc74876c62cd5c69d97
mateo <mateosanchez.msl@gmail.com>
2024-10-12 10:04:18 +0100

    about page fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,14 @@
+import { CodeIcon } from "@radix-ui/react-icons";
 import React from "react";
 
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="py-28 lg:px-20 px-10 lg:text-3xl text-xl flex flex-row items-center ">
+        currently under construction...
+      </div>
+    </div>
+  );
 }
 
 export default AboutPage;
