e84d9438e51ff30de5f4e127cdcdc95d436d3bdc
mateo <mateosanchez.msl@gmail.com>
2024-10-12 10:04:57 +0100

    about page fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function AboutPage() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="py-28 lg:px-20 px-10 lg:text-3xl text-xl flex flex-row items-center ">
+      <div className="py-28 lg:px-20 px-10 lg:text-4xl text-xl flex flex-row items-center ">
         currently under construction...
       </div>
     </div>
