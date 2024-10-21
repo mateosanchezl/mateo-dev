7329128276df5ef1c94fb6b2fb2863160ea7d97a
mateo <mateosanchez.msl@gmail.com>
2024-10-21 16:13:45 +0100

    system default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={afacad.className}>
-        <ThemeProvider attribute="class" defaultTheme="light">
+        <ThemeProvider attribute="class" defaultTheme="system">
           <Header />
           <main>{children}</main>
         </ThemeProvider>
