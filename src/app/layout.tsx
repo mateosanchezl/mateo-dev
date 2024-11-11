e29ada3f6ac9eebda3bff5b82d1c4edc59cf809e
mateo <mateosanchez.msl@gmail.com>
2024-11-11 18:33:03 +0000

    SEO improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,28 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Header from "@/components/header";
 
 export const metadata: Metadata = {
   title: "mateo sanchez",
-  description: "bolivian software engineering student and lab demonstrator at manchester met.",
+  description: "software engineering student & teaching assistant at manchester met.",
+  keywords: "mateo sanchez, software engineer, bolivian, manchester met, portfolio, projects",
+  openGraph: {
+    title: "mateo sanchez",
+    description: "software engineering student & teaching assistant at manchester met.",
+    url: "https://mateocodes.com",
+    siteName: "mateo sanchez portfolio",
+    locale: "en_GB",
+    type: "website",
+  },
+  twitter: {
+    card: "summary",
+    site: "@m1tweets1",
+    creator: "@m1tweets1",
+    title: "mateo sanchez",
+    description: "software engineering student & teaching assistant at manchester met.",
+    images: [],
+  },
+  metadataBase: new URL("https://mateocodes.com"),
 };
 
 export default function RootLayout({
